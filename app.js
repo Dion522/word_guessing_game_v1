4bb2874e0f0e22c763e29f41dac7b503a1598fd5
Dion522 <89555788+Dion522@users.noreply.github.com>
2022-04-06 19:35:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const qwerty = document.querySelectorAll('qwerty');
+const phrase = document.querySelector('phrase');
 let missed = 0;
 const startGame = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
@@ -23,9 +23,9 @@ const ul = document.querySelector('#phrase ul');
   } 
 // Function that loops through letters and creates a list item then appends to #phrase ul
   function addPhraseToDisplay (arr){
-      for(let i=0; i < phrases.length; i+=1 ){
+      for(let i=0; i < arr.length; i++ ){
     let li = document.createElement('li');
-    li.textContent = phrases[i];
+    li.textContent = arr[i];
     if(phrases[i] !== ' '){
         li.classList.add ('letter');
     }else {
@@ -35,11 +35,12 @@ const ul = document.querySelector('#phrase ul');
       
     
     }
-    const phraseArray = getRandomPhraseAsArray(phrases);
-    addPhrasetoDisplay(phraseArray);
-    
+     
 
   }
+
+  const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
  
   //Function that checks if button clicked matches letter from phrase array.
  function checkLetter(button){
@@ -61,13 +62,47 @@ const ul = document.querySelector('#phrase ul');
 
  } 
 //addEventListener that adds a class of "Chosen" to button so it can't be chosen twice and disables button.
- qwerty.addEventListener('click', (e) =>{
+// If checkLetter functions returns null, remove one of the tries channge liveHeart to lostHeart.png 
+qwerty.addEventListener('click', (e) =>{
     let buttonChosen = e.target;
     if(buttonChosen.tagName ==='button'){
         buttonChosen.classList.add('chosen');
         buttonChosen.disable = true;
 
         let letterFound= checkLetter(buttonChosen);
+
+        if(letterFound = 'null'){
+            const removeTry = document.querySelector('.tries');
+            removeTry.remove();
+            missed ++;
+            document.querySelector(".tries").src="images/lostHeart.png";
+            
+
+        }
     }
 
+    checkWin();
+
  });
+
+function checkWin(){
+    const letterClass = document.querySelector('.letter');
+    const showClass = document.querySelector('.show');
+
+    if(letterClass.length === showClass.length){
+        overlay.classList.add('win');
+        document.querySelector('.title').textContent = " You Win!";
+        overlay.style.display = 'flex'; 
+
+
+    }else {
+        overlay.classList.add('lose');
+        document.querySelector('.title').textContent = " You Win!";
+        overlay.style.display = 'flex'; 
+    }
+}
+
+
+
+
+
